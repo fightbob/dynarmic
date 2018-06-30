39acb129f334669235a39f233fb339a61234861f
MerryMage <MerryMage@users.noreply.github.com>
2018-06-30 12:12:52 +0100

    fixup! fixup! safe_ops

--- src/common/safe_ops.h
+++ src/common/safe_ops.h
@@ -14,6 +14,8 @@ namespace Dynarmic::Safe {
 
 template<typename T> T LogicalShiftLeft(T value, int shift_amount);
 template<typename T> T LogicalShiftRight(T value, int shift_amount);
+template<typename T> T ArithmeticShiftLeft(T value, int shift_amount);
+template<typename T> T ArithmeticShiftRight(T value, int shift_amount);
 
 template<typename T>
 T LogicalShiftLeft(T value, int shift_amount) {
@@ -60,6 +62,38 @@ inline u128 LogicalShiftRight(u128 value, int shift_amount) {
 template<typename T>
 T LogicalShiftRightDouble(T top, T bottom, int shift_amount) {
     return LogicalShiftLeft(top, int(Common::BitSize<T>()) - shift_amount) | LogicalShiftRight(bottom, shift_amount);
+}
+
+template<typename T>
+T ArithmeticShiftLeft(T value, int shift_amount) {
+    static_assert(std::is_integral_v<T>);
+
+    if (shift_amount >= static_cast<int>(Common::BitSize<T>())) {
+        return 0;
+    }
+
+    if (shift_amount < 0) {
+        return ArithmeticShiftRight(value, -shift_amount);
+    }
+
+    auto signed_value = static_cast<std::make_signed_t<T>>(value);
+    return static_cast<T>(signed_value << shift_amount);
+}
+
+template<typename T>
+T ArithmeticShiftRight(T value, int shift_amount) {
+    static_assert(std::is_integral_v<T>);
+
+    if (shift_amount >= static_cast<int>(Common::BitSize<T>())) {
+        return Common::MostSignificantBit(value) ? ~static_cast<T>(0) : 0;
+    }
+
+    if (shift_amount < 0) {
+        return ArithmeticShiftLeft(value, -shift_amount);
+    }
+
+    auto signed_value = static_cast<std::make_signed_t<T>>(value);
+    return static_cast<T>(signed_value >> shift_amount);
 }
 
 } // namespace Dynarmic::Safe
